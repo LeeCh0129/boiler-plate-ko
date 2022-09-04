3b2becfb4aad45881726e56348a1657471936dd3
LeeCh0129 <cksgh5477@gmail.com>
2022-09-05 02:42:52 +0900

    Auth 기능

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
-
+const { auth } = require('./middleware/auth');
 const { User } = require("./models/User");
 
 // bodyParser가 클라이언트에서 오는 정보를 서버에서 분석해서 가져올 수 있게함.
@@ -28,7 +28,7 @@ mongoose.connect(config.mongoURI, {
 app.get('/', (req, res) => res.send('Hello World!'))
 
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     // 회원 가입 할때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어줌.
     
     const user = new User(req.body)
@@ -41,7 +41,7 @@ app.post('/register', (req, res) => {
     })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
 
     // 요청된 이메일을 데이터베이스에서 있는지 찾음.
     User.findOne({ email: req.body.email }, (err, user) => {
@@ -69,6 +69,23 @@ app.post('/login', (req, res) => {
 
             })
         })
+    })
+})
+
+// role 1 어드민    role 2 특정 부서 어드민
+// role 0 -> 일반유저    role 0이 아니면 관리자
+app.get('/api/users/auth', auth, (req, res) => {
+
+    // 여기까지 미들웨어를 통과해 왔다는 이야기는 Authentication이 True 라는 뜻.
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
     })
 })
 
